c6ac783c7c381482a4231b5390a512d66377eb92
Andreas Enge <andreas.enge@inria.fr>
2021-04-29 19:28:35 +0200

    Use a PARI function for roots of class polynomials.
    * configure.ac: Increase the minimal PARI version to 2.11 and the minimal
      MPFRCX version to 0.6.1.
    * doc/cm.texi: Document the increased version requirements.
    * doc/version.texi: Update the date.
    * lib/class/pari.c (cm_pari_oneroot): Call the PARI function
      FpX_oneroot_split, which is inspired by the previous implementation.
      (cm_pari_onefactor, good_root_of_unity, ZX_get_mpzx): Remove static
      functions.

--- lib/class/pari.c
+++ lib/class/pari.c
@@ -27,9 +27,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 static GEN mpz_get_Z (mpz_t z);
 static void Z_get_mpz (mpz_t z, GEN x);
 static GEN mpzx_get_FpX (mpz_t *f, int deg, mpz_t p);
-static int ZX_get_mpzx (mpz_t *res, GEN f);
-static void cm_pari_onefactor (mpz_t *res, mpz_t *f, int deg, int deg_factor,
-   mpz_t p, bool verbose);
 
 /*****************************************************************************/
 /*                                                                           */
@@ -136,23 +133,6 @@ static GEN mpzx_get_FpX (mpz_t *f, int deg, mpz_t p)
    return res;
 }
 
-/*****************************************************************************/
-
-static int ZX_get_mpzx (mpz_t *res, GEN f)
-   /* f is a GEN of type t_POL over t_INT                                    */
-   /* The function returns the degree of f and, via res, the array of its    */
-   /* coefficients. res must contain sufficient space to hold all these, and */
-   /* its entries must already be initialised with mpz_init.                 */
-
-{
-   int i;
-   int deg = degree (f);
-
-   for (i = 0; i <= deg; i++)
-      Z_get_mpz (res [i], gel (f, i+2));
-
-   return deg;
-}
 
 /*****************************************************************************/
 /*                                                                           */
@@ -160,123 +140,7 @@ static int ZX_get_mpzx (mpz_t *res, GEN f)
 /*                                                                           */
 /*****************************************************************************/
 
-static GEN good_root_of_unity (int *n, const GEN p, const int deg,
-   const int deg_factor)
-   /* returns a root of unity in F_p that is suitable for finding a factor   */
-   /* of degree deg_factor of a polynomial of degree deg; the order is       */
-   /* returned in n                                                          */
-   /* A good choice seems to be n close to deg/deg_factor; we choose n       */
-   /* twice as big and decrement until it divides p-1.                       */
-
-{
-   GEN pm, factn, power, base, zeta;
-
-   pari_sp lbot, ltop = avma;
-
-   pm = subis (p, 1ul);
-   for (*n = deg / 2 / deg_factor + 1; !dvdiu (pm, *n); (*n)--);
-
-   factn = Z_factor (stoi (*n));
-   power = diviuexact (pm, *n);
-   base = gen_1;
-   do {
-      base = addis (base, 1l);
-      zeta = Fp_pow (base, power, p);
-      lbot = avma; /* memorise the only interesting object */
-   }
-   while (!equaliu (Fp_order (zeta, factn, p), *n));
-
-   avma = lbot;
-   zeta = gerepileuptoint (ltop, zeta);
-   return zeta;
-}
-
-/*****************************************************************************/
-
-static void cm_pari_onefactor (mpz_t *res, mpz_t *f, int deg, int deg_factor,
-   mpz_t p, bool verbose)
-   /* Assuming that deg_factor is the minimal degree of a factor of f, finds */
-   /* such a factor and returns it in res. The coefficients of res need to   */
-   /* already be initialised.                                                */
-
-{
-   GEN pp;
-   int n, deg_f, target, i;
-   GEN prim, expo, fact, minfactor, xplusa, zeta, xpow, tmp;
-   cm_timer clock, clock2;
-   pari_sp av;
-
-   cm_timer_start (clock);
-   if (verbose)
-      printf ("--- Factor finding, degree %i out of %i\n", deg_factor, deg);
-
-   pari_init (500000, 0);
-   paristack_setsize (500000, 500000000);
-   av = avma;
-
-   pp = mpz_get_Z (p);
-   fact = mpzx_get_FpX (f, deg, p);
-   minfactor = fact; /* factor of minimal degree found so far */
-
-   xplusa = pol_x (varn (fact));
-   while (degpol (minfactor) != deg_factor) {
-      /* split minfactor by computing its gcd with (X+a)^exp-zeta, where
-         zeta varies over the n-th roots of unity in F_p for a suitable n */
-      fact = FpX_normalize (minfactor, pp);
-      deg_f = degpol (fact);
-      prim = good_root_of_unity (&n, pp, deg_f, deg_factor);
-      expo = diviuexact (subis (powiu (pp, (unsigned long int) deg_factor), 1),
-                         (unsigned long int) n);
-      /* update X+a, avoid a=0 */
-      gel (xplusa, 2) = addis (gel (xplusa, 2), 1);
-      cm_timer_start (clock2);
-      xpow = FpXQ_pow (xplusa, expo, fact, pp);
-      cm_timer_stop (clock2);
-      if (verbose)
-         printf ("- Time for pow with n=%i in degree %i: %.1f\n",
-                 n, deg_f, cm_timer_get (clock2));
-      if (degpol (xpow) > 0 && degpol (xpow) < deg_f) {
-         /* Fix a target for early abort, depending on n and deg_f, to
-            avoid computing too many gcds; we stop as soon as a degree of
-            at most target is reached. It is necessary to have
-            target <= deg_f - 1. */
-         target = (2 * deg_f) / n - 1;
-         for (i = 0, zeta = gen_1;
-            i < n
-               && degree (minfactor) > deg_factor
-               /* stop early to avoid too many gcds */
-               && degree (minfactor) > target;
-            i++, zeta = Fp_mul (zeta, prim, pp)) {
-            cm_timer_start (clock2);
-            tmp = FpX_gcd (FpX_Fp_sub (xpow, zeta, pp), fact, pp);
-            cm_timer_stop (clock2);
-            if (verbose)
-               printf ("- Time for gcd of degrees %li, %li -> %li: %.1f\n",
-                  degpol (fact), degpol (xpow), degpol (tmp),
-                  cm_timer_get (clock2));
-            if (degpol (tmp) > 0 && degpol (tmp) < degpol (fact)) {
-               fact = FpX_div (fact, tmp, pp);
-               if (degpol (tmp) < degpol (minfactor))
-                  minfactor = tmp;
-            }
-         }
-      }
-    }
-
-   ZX_get_mpzx (res, FpX_normalize (minfactor, pp));
-
-   avma = av;
-   pari_close ();
-
-   cm_timer_stop (clock);
-   if (verbose)
-      printf ("-- Time for factor: %.1f\n", cm_timer_get (clock));
-}
-
-/*****************************************************************************/
-
-#if 0
-void cm_pari_oneroot_alt (mpz_t root, mpz_t *f, int deg, mpz_t p, bool verbose)
+void cm_pari_oneroot (mpz_t root, mpz_t *f, int deg, mpz_t p, bool verbose)
    /* finds a root of the polynomial f of degree deg over the prime field of */
    /* characteristic p, assuming that such a root exists, and returns it in  */
    /* the variable of the same name                                          */
@@ -294,7 +158,7 @@ void cm_pari_oneroot_alt (mpz_t root, mpz_t *f, int deg, mpz_t p, bool verbose)
    pp = mpz_get_Z (p);
    fp = mpzx_get_FpX (f, deg, p);
 
-   rootp = FpX_oneroot (fp, pp);
+   rootp = FpX_oneroot_split (fp, pp);
    Z_get_mpz (root, rootp);
 
    pari_close ();
@@ -302,28 +166,6 @@ void cm_pari_oneroot_alt (mpz_t root, mpz_t *f, int deg, mpz_t p, bool verbose)
    cm_timer_stop (clock);
    if (verbose)
       printf ("-- Time for root: %.1f\n", cm_timer_get (clock));
-}
-#endif
-
-/*****************************************************************************/
-
-void cm_pari_oneroot (mpz_t root, mpz_t *f, int deg, mpz_t p, bool verbose)
-   /* finds a root of the polynomial f of degree deg over the prime field of */
-   /* characteristic p, assuming that such a root exists, and returns it in  */
-   /* the variable of the same name                                          */
-
-{
-   mpz_t fact [2];
-
-   mpz_init (fact [0]);
-   mpz_init (fact [1]);
-   cm_pari_onefactor (fact, f, deg, 1, p, verbose);
-   if (mpz_sgn (fact [0]) != 0)
-      mpz_sub (root, p, fact [0]);
-   else
-      mpz_set_ui (root, 0ul);
-   mpz_clear (fact [0]);
-   mpz_clear (fact [1]);
 }
 
 /*****************************************************************************/
