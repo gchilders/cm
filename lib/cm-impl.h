e6ef0077d8af345698337df94c1468f357f9987a
Andreas Enge <andreas.enge@inria.fr>
2022-07-13 15:11:49 +0200

    Replace delegation of Cornacchia's algorithm to PARI by a new function.
    The new function should use some fewer multiplications, but supposedly all
    time is spent in halfgcd inside PARI anyway. For 10^3000+1027 on 640 cores,
    both implementations take 24s.
    
    * lib/cm-impl.h (cm_nt_mpz_cornacchia): Remove function declaration.
    * lib/nt.c (cm_nt_mpz_cornacchia): Remove function.
    * lib/pari.c (cm_pari_cornacchia): Replace the complete delegation to the
      cornacchia2_sqrt function of PARI to an implementation that uses only
      the function halfgcd from PARI.

--- lib/cm-impl.h
+++ lib/cm-impl.h
@@ -97,8 +97,6 @@ extern void cm_nt_mpz_tonelli_si_with_generator (mpz_ptr root,
 extern void cm_nt_mpz_tonelli (mpz_ptr root, mpz_srcptr a, mpz_srcptr p);
 extern void cm_nt_mpz_tonelli_si (mpz_ptr root, const long int a,
    mpz_srcptr p);
-extern bool cm_nt_mpz_cornacchia (mpz_ptr t, mpz_ptr v, mpz_srcptr p,
-   mpz_srcptr root, const int_cl_t d);
 extern void cm_nt_mpz_tree_gcd (mpz_t *gcd, mpz_srcptr n, mpz_t *m,
    int no_m);
 extern bool cm_nt_cget_zz (mpz_ptr out1, mpz_ptr out2, ctype in, ctype omega);
