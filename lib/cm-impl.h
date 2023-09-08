c21407d5883f3c0d85a6af1384f6a465302ed61d
Andreas Enge <andreas.enge@inria.fr>
2023-09-08 12:08:18 +0200

    Add header inclusion.
    * cm-impl.h: Include stdarg.h, which may be required for va_start
      and va_end.

--- lib/cm-impl.h
+++ lib/cm-impl.h
@@ -31,6 +31,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #include <math.h>
 #include <ctype.h>
 #include <stdint.h>
+#include <stdarg.h>
 #ifdef WITH_MPI
 #include <unistd.h> /* for usleep */
 #include <mpi.h>
