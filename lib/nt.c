b8212d6407624c9387fb7157a9356441e85a31e7
gchilders <gchilders@fullerton.edu>
2023-11-03 10:07:11 -0700

    Fix memory leak in gwnum code

--- lib/nt.c
+++ lib/nt.c
@@ -149,7 +149,7 @@ static void cm_mpz_powm (mpz_ptr z, mpz_srcptr a, mpz_srcptr e, mpz_srcptr p)
 #define SAFE 64 // Careful loops for large base loop.
 #define ROUND_OFF_LIMIT 0.35
 #define ZQ 17586324677945679473ULL // 64 bit prime for Gerbicz EDAC
-#define w2m(wx,gx,mx) gwtogiant(gwdata,wx,gx);gtompz(gx,mx);
+#define w2m(wx,gx,mx) gwtogiant(&gwdata,wx,gx);gtompz(gx,mx);
 
 /*****************************************************************************/
 
@@ -164,7 +164,7 @@ void small_base_exp ( mpz_ptr mr, double db, mpz_srcptr me, mpz_srcptr mn )
     char string [MAX_STRING_LENGTH];
     mpz_t zr, zn, zc, zrq;
     gwnum wr, wc;
-    gwhandle *gwdata;
+    gwhandle gwdata;
     giant gr, gn;
     mpz_init ( zr );
     mpz_init ( zn );
@@ -178,59 +178,58 @@ void small_base_exp ( mpz_ptr mr, double db, mpz_srcptr me, mpz_srcptr mn )
     gr  = newgiant ( g_length );
     gn  = newgiant ( g_length );
     ctog ( string, gn );
-    gwdata = (gwhandle*) malloc ( sizeof ( gwhandle ) );
-    gwinit ( gwdata );
-    gwset_maxmulbyconst ( gwdata, (long) db );
-    gwset_larger_fftlen_count ( gwdata, fft_size );
-    gwsetup_general_mod_giant ( gwdata, gn );
-    gwsetmulbyconst ( gwdata, (long) db );
-    wr = gwalloc ( gwdata );
-    wc = gwalloc ( gwdata );
-    gwerror_checking ( gwdata, 1 );
-    dbltogw ( gwdata, db, wr );
+    gwinit ( &gwdata );
+    gwset_maxmulbyconst ( &gwdata, (long) db );
+    gwset_larger_fftlen_count ( &gwdata, fft_size );
+    gwsetup_general_mod_giant ( &gwdata, gn );
+    gwsetmulbyconst ( &gwdata, (long) db );
+    wr = gwalloc ( &gwdata );
+    wc = gwalloc ( &gwdata );
+    gwerror_checking ( &gwdata, 1 );
+    dbltogw ( &gwdata, db, wr );
     mpz_set_ui ( zr, zb );
     for ( int i = mpz_sizeinbase ( me, 2 ) - 2; i >= 0; )
     {
         mpz_set ( zc, zr );
-        gwcopy ( gwdata, wr, wc );
+        gwcopy ( &gwdata, wr, wc );
         for ( j = 0; j < zstep && i >= 0; j++, i-- )
         {
             if ( mpz_tstbit ( me, i ) )
             {
-                gwsquare2 ( gwdata, wr, wr, GWMUL_MULBYCONST ); // From gwnum.h
+                gwsquare2 ( &gwdata, wr, wr, GWMUL_MULBYCONST ); // From gwnum.h
                 mpz_mul ( zr, zr, zr );
                 mpz_mul_ui ( zr, zr, zb );
                 mpz_mod_ui ( zr, zr, ZQ );
             }
             else
             {
-                gwsquare2 ( gwdata, wr, wr, 0x0000 );
+                gwsquare2 ( &gwdata, wr, wr, 0x0000 );
                 mpz_mul ( zr, zr, zr );
                 mpz_mod_ui ( zr, zr, ZQ );
             }
         }
         w2m ( wr, gr, mr );
         mpz_mod_ui ( zrq, mr, ZQ );
-        if ( gw_get_maxerr ( gwdata ) > ROUND_OFF_LIMIT || mpz_cmp ( zrq, zr ) != 0 )
+        if ( gw_get_maxerr ( &gwdata ) > ROUND_OFF_LIMIT || mpz_cmp ( zrq, zr ) != 0 )
         {
-//            printf ( "*** (sm) Upping FFT size due to round off error or Gerbicz error before bit %d.\n", i );
-            gwtogiant ( gwdata, wc, gr );
-            gwfreeall ( gwdata );
-            gwinit ( gwdata );
-            gwset_maxmulbyconst ( gwdata, (long) db );
-            gwset_larger_fftlen_count ( gwdata, ++fft_size );
-            gwsetup_general_mod_giant ( gwdata, gn );
-            gwsetmulbyconst ( gwdata, (long) db );
-            wr = gwalloc ( gwdata );
-            wc = gwalloc ( gwdata );
-            gwerror_checking ( gwdata, 1 );
-            gianttogw ( gwdata, gr, wr );
+	  //            printf ( "*** (sm) Upping FFT size due to round off error or Gerbicz error before bit %d.\n", i );
+            gwtogiant ( &gwdata, wc, gr );
+            gwdone ( &gwdata );
+            gwinit ( &gwdata );
+            gwset_maxmulbyconst ( &gwdata, (long) db );
+            gwset_larger_fftlen_count ( &gwdata, ++fft_size );
+            gwsetup_general_mod_giant ( &gwdata, gn );
+            gwsetmulbyconst ( &gwdata, (long) db );
+            wr = gwalloc ( &gwdata );
+            wc = gwalloc ( &gwdata );
+            gwerror_checking ( &gwdata, 1 );
+            gianttogw ( &gwdata, gr, wr );
             mpz_set ( zr, zc );
             i += j;
         }
     }
     w2m ( wr, gr, mr );
-    gwdone ( gwdata );
+    gwdone ( &gwdata );
     mpz_mod ( mr, mr, mn );
     mpz_clear ( zr );
     mpz_clear ( zn );
@@ -254,7 +253,7 @@ void medium_base_exp ( mpz_ptr mr, double db, mpz_srcptr me, mpz_srcptr mn )
     mpz_t zr, zn, zc, zrq;
     giant gr, gn;
     gwnum wr, wc;
-    gwhandle *gwdata;
+    gwhandle gwdata;
     mpz_init ( zr );
     mpz_init ( zn );
     mpz_init ( zc );
@@ -267,51 +266,50 @@ void medium_base_exp ( mpz_ptr mr, double db, mpz_srcptr me, mpz_srcptr mn )
     gr = newgiant ( g_length );
     gn = newgiant ( g_length );
     ctog ( string, gn );
-    gwdata = (gwhandle*) malloc ( sizeof ( gwhandle ) );
-    gwinit ( gwdata );
-    gwset_larger_fftlen_count ( gwdata, fft_size );
-    gwsetup_general_mod_giant ( gwdata, gn );
-    wr = gwalloc ( gwdata );
-    wc = gwalloc ( gwdata );
-    gwerror_checking ( gwdata, 1 );
-    dbltogw ( gwdata, db, wr );
+    gwinit ( &gwdata );
+    gwset_larger_fftlen_count ( &gwdata, fft_size );
+    gwsetup_general_mod_giant ( &gwdata, gn );
+    wr = gwalloc ( &gwdata );
+    wc = gwalloc ( &gwdata );
+    gwerror_checking ( &gwdata, 1 );
+    dbltogw ( &gwdata, db, wr );
     mpz_set_ui ( zr, zb );
     for ( int i = mpz_sizeinbase ( me, 2 ) - 2 ; i >= 0; )
     {
-        gwcopy ( gwdata, wr, wc );
+        gwcopy ( &gwdata, wr, wc );
         mpz_set ( zc, zr );
         for ( j = 0; j < zstep && i >= 0; j++, i-- )
         {
-            gwsquare2 ( gwdata, wr, wr, 0x0000 );
+            gwsquare2 ( &gwdata, wr, wr, 0x0000 );
             mpz_mul ( zr, zr, zr );
             mpz_mod_ui ( zr, zr, ZQ );
             if ( mpz_tstbit ( me, i ) )
             {
-                gwsmallmul ( gwdata, db, wr );
+                gwsmallmul ( &gwdata, db, wr );
                 mpz_mul_ui ( zr, zr, zb );
                 mpz_mod_ui ( zr, zr, ZQ );
             }
         }
         w2m ( wr, gr, mr );
         mpz_mod_ui ( zrq, mr, ZQ );
-        if ( gw_get_maxerr ( gwdata ) > ROUND_OFF_LIMIT || mpz_cmp ( zrq, zr ) != 0 )
+        if ( gw_get_maxerr ( &gwdata ) > ROUND_OFF_LIMIT || mpz_cmp ( zrq, zr ) != 0 )
         {
- //           printf ( "*** (md) Upping FFT size due to round off error or Gerbicz error before bit %d.\n", i );
-            gwtogiant ( gwdata, wc, gr );
-            gwfreeall ( gwdata );
-            gwinit ( gwdata );
-            gwset_larger_fftlen_count ( gwdata, ++fft_size );
-            gwsetup_general_mod_giant ( gwdata, gn );
-            wr = gwalloc ( gwdata );
-            wc = gwalloc ( gwdata );
-            gwerror_checking ( gwdata, 1 );
-            gianttogw ( gwdata, gr, wr );
+	  //            printf ( "*** (md) Upping FFT size due to round off error or Gerbicz error before bit %d.\n", i );
+            gwtogiant ( &gwdata, wc, gr );
+            gwdone ( &gwdata );
+            gwinit ( &gwdata );
+            gwset_larger_fftlen_count ( &gwdata, ++fft_size );
+            gwsetup_general_mod_giant ( &gwdata, gn );
+            wr = gwalloc ( &gwdata );
+            wc = gwalloc ( &gwdata );
+            gwerror_checking ( &gwdata, 1 );
+            gianttogw ( &gwdata, gr, wr );
             mpz_set ( zr, zc );
             i += j;
         }
     }
     w2m ( wr, gr, mr );
-    gwdone ( gwdata );
+    gwdone ( &gwdata );
     mpz_mod ( mr, mr, mn );
     mpz_clear ( zr );
     mpz_clear ( zn );
@@ -323,7 +321,7 @@ void medium_base_exp ( mpz_ptr mr, double db, mpz_srcptr me, mpz_srcptr mn )
 
 /*****************************************************************************/
 
-void large_base_exp ( mpz_ptr mr, mpz_ptr mb, mpz_srcptr me, mpz_srcptr mn )
+void large_base_exp ( mpz_ptr mr, mpz_srcptr mb, mpz_srcptr me, mpz_srcptr mn )
 {
     int fft_size = INI_FFT_SIZE;
     int string_length;
@@ -335,7 +333,7 @@ void large_base_exp ( mpz_ptr mr, mpz_ptr mb, mpz_srcptr me, mpz_srcptr mn )
     mpz_t zr, zb, zn, zc, zrq;
     giant gr, gb, gn;
     gwnum wr, wb, wc;
-    gwhandle *gwdata;
+    gwhandle gwdata;
     mpz_init ( zr );
     mpz_init ( zn );
     mpz_init_set ( zb, mb );
@@ -351,74 +349,73 @@ void large_base_exp ( mpz_ptr mr, mpz_ptr mb, mpz_srcptr me, mpz_srcptr mn )
     gb = newgiant ( g_length );
     gn = newgiant ( g_length );
     ctog ( string, gn );
-    gwdata = (gwhandle*) malloc ( sizeof ( gwhandle ) );
-    gwinit ( gwdata );
-    gwset_larger_fftlen_count ( gwdata, fft_size );
-    gwsetup_general_mod_giant ( gwdata, gn );
-    wr = gwalloc ( gwdata );
-    wb = gwalloc ( gwdata );
-    wc = gwalloc ( gwdata );
-    gwerror_checking ( gwdata, 1 );
+    gwinit ( &gwdata );
+    gwset_larger_fftlen_count ( &gwdata, fft_size );
+    gwsetup_general_mod_giant ( &gwdata, gn );
+    wr = gwalloc ( &gwdata );
+    wb = gwalloc ( &gwdata );
+    wc = gwalloc ( &gwdata );
+    gwerror_checking ( &gwdata, 1 );
     mpztog ( mb, gb );
-    gianttogw ( gwdata, gb, wb );
-    gwcopy ( gwdata, wb, wr );
+    gianttogw ( &gwdata, gb, wb );
+    gwcopy ( &gwdata, wb, wr );
     mpz_set ( zr, zb );
     for ( int i = j; i > k; i-- )
     {
-        gwmul3_carefully ( gwdata, wr, wr, wr, 0x0000 );
+        gwmul3_carefully ( &gwdata, wr, wr, wr, 0x0000 );
         mpz_mul ( zr, zr, zr );
         mpz_mod_ui ( zr, zr, ZQ );
         if ( mpz_tstbit ( me, i ) )
         {
-            gwmul_carefully ( gwdata, wb, wr );
+            gwmul_carefully ( &gwdata, wb, wr );
             mpz_mul ( zr, zr, zb );
             mpz_mod_ui ( zr, zr, ZQ );
         }
     }
     for ( int i = k ; i > SAFE; )
     {
-        gwcopy ( gwdata, wr, wc );
+        gwcopy ( &gwdata, wr, wc );
         mpz_set ( zc, zr );
         for ( j = 0; j < zstep && i > SAFE; j++, i-- )
         {
-            gwsquare2 ( gwdata, wr, wr, 0x0000 );
+            gwsquare2 ( &gwdata, wr, wr, 0x0000 );
             mpz_mul ( zr, zr, zr );
             mpz_mod_ui ( zr, zr, ZQ );
             if ( mpz_tstbit ( me, i ) )
             {
-                gwmul3 ( gwdata, wb, wr, wr, 0x0002 );
+                gwmul3 ( &gwdata, wb, wr, wr, 0x0002 );
                 mpz_mul (zr, zr, zb );
                 mpz_mod_ui ( zr, zr, ZQ );
             }
         }
         w2m ( wr, gr, mr );
         mpz_mod_ui ( zrq, mr, ZQ );
-        if ( gw_get_maxerr ( gwdata ) > ROUND_OFF_LIMIT || mpz_cmp ( zrq, zr ) != 0 )
+        if ( gw_get_maxerr ( &gwdata ) > ROUND_OFF_LIMIT || mpz_cmp ( zrq, zr ) != 0 )
         {
-//            printf ( "*** (lg) Upping FFT size due to round off error or Gerbicz error before bit %d.\n", i );
-            gwtogiant ( gwdata, wc, gr );
-            gwfreeall ( gwdata );
-            gwinit ( gwdata );
-            gwset_larger_fftlen_count ( gwdata, ++fft_size );
-            gwsetup_general_mod_giant ( gwdata, gn );
-            wr = gwalloc ( gwdata );
-            wb = gwalloc ( gwdata );
-            wc = gwalloc ( gwdata );
-            gwerror_checking ( gwdata, 1 );
-            gianttogw ( gwdata, gb, wb );
-            gianttogw ( gwdata, gr, wr );
+	  //            printf ( "*** (lg) Upping FFT size due to round off error or Gerbicz error before bit %d.\n", i );
+            gwtogiant ( &gwdata, wc, gr );
+            gwdone ( &gwdata );
+            gwinit ( &gwdata );
+            gwset_larger_fftlen_count ( &gwdata, ++fft_size );
+            gwsetup_general_mod_giant ( &gwdata, gn );
+            wr = gwalloc ( &gwdata );
+            wb = gwalloc ( &gwdata );
+            wc = gwalloc ( &gwdata );
+            gwerror_checking ( &gwdata, 1 );
+            gianttogw ( &gwdata, gb, wb );
+            gianttogw ( &gwdata, gr, wr );
             mpz_set ( zr, zc );
             i += j;
         }
     }
     for ( int i = SAFE; i >= 0; i-- )
     {
-        gwmul3_carefully ( gwdata, wr, wr, wr, 0x0000 );
+        gwmul3_carefully ( &gwdata, wr, wr, wr, 0x0000 );
         mpz_mul ( zr, zr, zr );
         mpz_mod_ui ( zr, zr, ZQ );
         if ( mpz_tstbit ( me, i ) )
         {
-            gwmul_carefully ( gwdata, wb, wr );
+            gwmul_carefully ( &gwdata, wb, wr );
             mpz_mul ( zr, zr, zb );
 	    mpz_mod_ui ( zr, zr, ZQ );
         }
@@ -427,10 +424,10 @@ void large_base_exp ( mpz_ptr mr, mpz_ptr mb, mpz_srcptr me, mpz_srcptr mn )
     mpz_mod_ui ( zrq, mr, ZQ );
     if ( mpz_cmp ( zrq, zr ) != 0 )
     {
-	printf ( "Uh oh!!\n" );
-	exit ( -1 );
+	   printf ( "Uh oh!!\n" );
+	   exit ( -1 );
     }
-    gwdone ( gwdata );
+    gwdone ( &gwdata );
     mpz_mod ( mr, mr, mn );
     mpz_clear ( zr );
     mpz_clear ( zn );
@@ -445,9 +442,17 @@ void large_base_exp ( mpz_ptr mr, mpz_ptr mb, mpz_srcptr me, mpz_srcptr mn )
 
 int gw_prp ( mpz_srcptr mn )
 { // base 3 euler
-    if ( mpz_sizeinbase ( mn, 2 ) < GW_THRESHOLD )
+    int size = mpz_sizeinbase ( mn, 2 );
+    if ( size < GW_THRESHOLD )
     {
-        return ( mpz_probab_prime_p ( mn, 0) > 0 );
+      if ( size < 3000 )
+	   {
+	      return ( mpz_probab_prime_p ( mn, 0 ) > 0 );
+	   }
+      else
+	   {
+	      return ( mpz_millerrabin ( mn, 1 ) > 0 );
+	   }
     }
     int res = 0;
     mpz_t mr;
